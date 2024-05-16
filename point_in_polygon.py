baff2f2dc34216c880d2a1721d64efdbefb300af
byungsoo <byung@bu.edu>
2024-05-16 09:40:03 +0900

    polygon

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -1,41 +1,3 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-# import supervision as sv
-from ultralytics import YOLO
-
-
-model = YOLO("yolov8n.pt")
-image = cv2.imread('people.png')
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# Run Detection
-results = model(image)[0]
-
-# initiate polygon zone
-polygon = ([
-    [1214, 0],
-    [831, 918],
-    [1110, 901],
-    [1368, 0]
-])
-
-poly = [(250, 100), (400, 200), (350, 360),
-            (150, 360), (100, 200)]
-
-isClosed = True
-color = (2550,0,0)
-thickness = 50
-poly_image = cv2.polylines(rgb_image, [polygon], True, color, thickness)
-
-plt.imshow(poly_image)
-
-%matplotlib qt
-
-
-'''
-https://gist.github.com/inside-code-yt/7064d1d1553a2ee117e60217cfd1d099
-'''
 from polygenerator import random_polygon
 import matplotlib.pyplot as plt
 
@@ -48,7 +10,6 @@ def is_inside(edges, xp, yp):
             cnt += 1
     return cnt%2 == 1
 
-is_inside(polygon, 800,100)
 
 def onclick(event):
     xp, yp = event.xdata, event.ydata
@@ -70,5 +31,3 @@ xs, ys = zip(*polygon)
 plt.gcf().canvas.mpl_connect('button_press_event', onclick)
 plt.plot(xs, ys, "b-", linewidth=0.8)
 plt.show()
-
-
